a65e5a411ae6b0472ca798ed2b2bf7478ae44090
Adrian Pistol <vifino@tty.sh>
2016-06-25 20:46:27 +0200

    Improved tests to test sql backend, because that one we can test purely in memory.

--- spec/carbon_spec.lua
+++ spec/carbon_spec.lua
@@ -17,8 +17,28 @@ describe("carbonvfs", function()
 		if carbon then
 			it("should load carbon specific backends", function()
 				assert.equals(type(vfs.backends.physfs), "function")
-				assert.equals(type(vfs.backends.gofs), "function")
+				assert.equals(type(vfs.backends.sql), "function")
 				assert.equals(type(vfs.backends.shared), "function")
+			end)
+			it("should be able to use the sql backend", function()
+				local db = assert(sql.open("ql-mem", "mem"))
+				vfs.new("sqltest", "sql", db, {
+					tablename = "files",
+				})
+
+				local teststr = "Hello World!"
+				
+				assert(vfs.write("sqltest:/test.txt", teststr))
+				local str = assert(vfs.read("sqltest:/test.txt"))
+				assert.equals(str, teststr)
+
+				local size = assert(vfs.size("sqltest:/test.txt"))
+				assert.equals(size, #teststr)
+
+				local list = assert(vfs.list("sqltest:/"))
+				assert.equals(list[1], "test.txt")
+
+				db:close()
 			end)
 		else
 			it("should work flawlessly", function()
