795b71d64529455442f0d86a5ec338bde08a66d6
Adrian Pistol <vifino@tty.sh>
2016-06-27 23:59:12 +0200

    Test shared backend.

--- backends/carbon/shared.lua
+++ backends/carbon/shared.lua
@@ -13,12 +13,13 @@ end
 return {
 	shared = function(drivename, sharedbackend, ...)
 		local msgpack = require("msgpack")
-		local kvstore_key_base = "carbon:vfs:"..drivename..":"
+		local kvstore_key_base = "carbon:vfs:driver:shared:"..drivename..":"
 		if not sharedbackend then
-			if not kvstore._get(kvstore_key_base.."com") then
+			local initstatus = kvstore._get(kvstore_key_base.."initialized")
+			if initstatus ~= true then
 				e("Shared backend has not been initialized for drive "..drivename)
 			end
-				local function call(name, ...)
+			local function call(name, ...)
 				local shrd = kvstore._get(kvstore_key_base.."com")
 				com.send(shrd, msgpack.pack({
 					method = name,
@@ -28,21 +29,26 @@ return {
 				local res = msgpack.unpack(com.receive(shrd))
 				if res[1] == false then
 					error(res[2], 0)
-				elseif res[1] == true then
+				elseif res[1] == true and res[2] then
 					return unpack(res, 2, res.n)
 				end
 			end
+
 			local tmp = {
 				unmount = function(...)
-					call("unmount")
-					kvstore._del(kvstore_key_base.."com")
-					kvstore._del(kvstore_key_base.."args")
+					if sharedbackend == false then
+						call("unmount")
+						kvstore._del(kvstore_key_base.."com")
+						kvstore._del(kvstore_key_base.."args")
+						kvstore._del(kvstore_key_base.."methods")
+						kvstore._del(kvstore_key_base.."initialized")
+					end
 				end
 			}
-			for k, v in pairs(luar.slice2table(kvstore._get(kvstore_key_base.."methods"))) do
-				if not k == "unmount" then
-					tmp[v] = function(...)
-						return call(v, ...)
+			for _, k in pairs(luar.slice2table(kvstore._get(kvstore_key_base.."methods"))) do
+				if k ~= "unmount" then
+					tmp[k] = function(...)
+						return call(k, ...)
 					end
 				end
 			end
@@ -54,19 +60,23 @@ return {
 				local msgpack = require("msgpack")
 				local bargs = msgpack.unpack(kvstore._get(kvstore_key_base.."args"))
 				--require("vfs") -- already loaded by default
-				local drive = vfs_backend(drivename, unpack(bargs, 1, bargs.n))
+				local suc, drive = pcall(vfs_backend, drivename, unpack(bargs, 1, bargs.n))
+				if not suc then
+					com.send(threadcom, tostring(drive))
+					return
+				end
 
 				local methodlist = {}
 				for k, _ in pairs(drive) do
 					table.insert(methodlist, k)
 				end
 				kvstore._set(kvstore_key_base.."methods", methodlist)
-				com.send(threadcom, true) -- indicate that we are done initializing
+				com.send(threadcom, "") -- indicate that we are done initializing
 
 				while true do
 					local src = com.receive(threadcom)
 					local cmd = msgpack.unpack(src)
-						local name, args = cmd.method, cmd.args
+					local name, args = cmd.method, cmd.args
 					local res = table.pack(pcall(drive[name], unpack(args, 1, args.n)))
 					com.send(threadcom, msgpack.pack(res))
 					if name == "unmount" then -- I guess it is time to go.
@@ -74,9 +84,13 @@ return {
 					end
 				end
 			end)
-			com.receive(shrd) -- block until thread is done with init
+			local err = com.receive(shrd) -- block until thread is done with init
+			if err and err ~= "" then
+				error(err, 0)
+			end
 			kvstore._set(kvstore_key_base.."com", shrd)
-			return vfs.backends.shared(drivename)
+			kvstore._set(kvstore_key_base.."initialized", true)
+			return vfs.backends.shared(drivename, false)
 		end
 	end,
 }
--- spec/carbon_spec.lua
+++ spec/carbon_spec.lua
@@ -10,35 +10,81 @@ say:set("assertion.type.negative", "Expected %s to not be type %s")
 assert:register("assertion", "type", is_type, "assertion.is_type.postitive", "assertion.is_type.negative")
 --]]
 
+local function loadvfs()
+	package.loaders[1] = function(modname)
+		local fh, err = io.open("./"..modname:gsub("%.", "/")..".lua", "r")
+		if err then
+			return "\n\tnot found using temp loader"
+		end
+		local src = fh:read("*all")
+		fh:close()
+		return load(src)
+	end
+	vfs = dofile("init.lua")
+	return vfs
+end
+
+local function basicvfstest(drive)
+	local eq = function(a, b)
+		if type(assert) == "function" then
+			assert(a == b)
+		else
+			assert.equals(a, b)
+		end
+	end
+
+	local teststr = "Hello World!"
+
+	assert(vfs.write(drive..":/test.txt", teststr))
+	local str = assert(vfs.read(drive..":/test.txt"))
+	eq(str, teststr)
+
+	local size = assert(vfs.size(drive..":/test.txt"))
+	eq(size, #teststr)
+
+	local list = assert(vfs.list(drive..":/"))
+	eq(list[1], "test.txt")
+end
+
 -- Actual tests
 describe("carbonvfs", function()
-	local vfs = dofile("init.lua")
 	describe("should run under carbon and it", function()
 		if carbon then
+			loadvfs()
 			it("should load carbon specific backends", function()
 				assert.equals(type(vfs.backends.physfs), "function")
 				assert.equals(type(vfs.backends.sql), "function")
 				assert.equals(type(vfs.backends.shared), "function")
 			end)
-			it("should be able to use the sql backend", function()
-				local db = assert(sql.open("ql-mem", "mem"))
-				vfs.new("sqltest", "sql", db, {
-					tablename = "files",
-				})
+			describe("should be able to use the sql backend", function()
+				it("normally", function()
+					local db = assert(sql.open("ql-mem", "sqltest"))
+					vfs.new("sqltest", "sql", db, {
+						tablename = "files",
+					})
 
-				local teststr = "Hello World!"
-				
-				assert(vfs.write("sqltest:/test.txt", teststr))
-				local str = assert(vfs.read("sqltest:/test.txt"))
-				assert.equals(str, teststr)
+					basicvfstest("sqltest")
 
-				local size = assert(vfs.size("sqltest:/test.txt"))
-				assert.equals(size, #teststr)
+					db:close()
+				end)
+				it("using the shared backend", function()
+					local db = assert(sql.open("ql-mem", "sqltestshared"))
+					vfs.new("sqlsharedtest", "shared", "sql", db, {
+						tablename = "files",
+					})
 
-				local list = assert(vfs.list("sqltest:/"))
-				assert.equals(list[1], "test.txt")
+					basicvfstest("sqlsharedtest")
 
-				db:close()
+					local waiter = thread.run(function()
+						loadvfs()
+						vfs.new("sqlsharedtest", "shared")
+
+						basicvfstest("sqlsharedtest")
+					end)
+
+					waiter()
+					db:close()
+				end)
 			end)
 		else
 			it("should work flawlessly", function()
@@ -47,4 +93,3 @@ describe("carbonvfs", function()
 		end
 	end)
 end)
-
