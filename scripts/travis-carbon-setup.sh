ff16ba83813ae1f551c427d7f15b37705d7542c4
Adrian Pistol <vifino@tty.sh>
2016-07-06 04:53:32 +0200

    More travis stuff. This is starting to get on my nerves.

--- scripts/travis-carbon-setup.sh
+++ scripts/travis-carbon-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+set -e 
 if [ "$1" == "YES" ]; then
+	echo "Downloading carbon..."
 	curl $(curl https://apps.wtfits.science/latest-carbon) > lua_install/bin/lua
 	chmod +x lua_install/bin/lua
+	echo "Done."
 fi
