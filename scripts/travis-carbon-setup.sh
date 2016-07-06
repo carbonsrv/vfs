e23a3113ac0ff97c7c1f58dc3dbdf0662d54b004
Adrian Pistol <vifino@tty.sh>
2016-07-06 04:56:46 +0200

    Okay. How hard can this be?

--- scripts/travis-carbon-setup.sh
+++ scripts/travis-carbon-setup.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 set -e 
-if [ "$1" == "YES" ]; then
+if [[ "$1" == "YES" ]]; then
 	echo "Downloading carbon..."
 	curl $(curl https://apps.wtfits.science/latest-carbon) > lua_install/bin/lua
 	chmod +x lua_install/bin/lua
